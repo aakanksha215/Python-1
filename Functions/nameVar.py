9c14c489fd5d68de22333dec8324c4968c546ee6
Aakanksha Chamoli <97774809+aakanksha215@users.noreply.github.com>
2022-10-16 18:15:53 +0530

    commit7

---
+++ Functions/nameVar.py
@@ -0,0 +1,10 @@
+
+# print("Hello "+ __name__)
+
+def main():
+    print('Hello')
+    print('Welcome')
+
+
+if __name__ == "__main()__":
+    main()
---
+++ Functions/specialVar.py
@@ -0,0 +1,8 @@
+
+# Special Variable - name
+# starting point of execution of program
+
+import nameVar
+
+
+print(__name__)
